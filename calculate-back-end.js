478506d16bf863c97a11ef1314026c3f342f91de
Calo Corp <marc.calo@codeimmersives.com>
2020-06-22 16:38:26 -0400

    passing all tests like a boss

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -2,9 +2,32 @@
  * YOUR CODE HERE *
  ******************/
 
+const calculate = function (str1, str2,str3){
+if (str3 === '+' || str3 === 'added to' || str3 === 'plus'){
 
- 
+  return Number(str1) + Number(str2)}   
 
+else if (str3 === '-'|| str3 === 'subtracted from'||  str3 === 'minus'){
+
+ return Number(str1) - Number(str2)
+
+
+}
+
+else if (str3 === 'X'|| str3 === 'multiplied by'||  str3 === 'x' || str3 === 'times'){
+
+  return Number(str2) * Number(str1)
+}
+else if (str3 === '/'|| str3 === 'divided by'){
+
+  return Number(str1) / Number(str2)}
+
+else if (str3 === '%'|| str3 === 'mod'||  str3 === 'modulus'){
+
+  return Number(str1) % Number(str2)
+}
+else return "Sorry, that's not a mathematical operation!"
+}
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
